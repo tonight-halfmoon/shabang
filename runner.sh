23e335a3068c8506f7a6eba01e78c78a7789b1e8
ahmad.elghafari@valtech.com <ahmad.elghafari@valtech.com>
2017-06-05 17:40:08 +0200

    polished for MacOS

--- runner.sh
+++ runner.sh
@@ -4,6 +4,7 @@ e_noArgs=59
 e_notFound=60
 e_notDirectory=61
 e_unable2Change2BaseDir=62
+e_unable2StartFrontEnd=63
 
 d_startedin=`pwd`
 echo "d_startedin= $d_startedin" >&2
@@ -12,18 +13,20 @@ c_pullFirst=0
 c_defaultGradleTasks="clean build"
 
 l_logfile=$d_startedin/script.log
+log_errors=$d_startedin/errors.log
 
 p_bootproject=unknown
 
 if [ $# -eq 0 ]
 then
-	echo "Usage: `basename $0` base directory options (-urpf) " >&2
-	echo "Incorrect usage" 1>$l_logfile
+	echo "Usage: `basename $0` <base directory>" >&2
+	echo "Incorrect usage" 2>>$log_errors
 	exit $e_noArgs
 fi
 
 cleanLogs () {
 	: > $l_logfile
+	: > $log_errors
 }
 
 checkProvidedDir () {
@@ -37,7 +40,7 @@ checkProvidedDir () {
 	if [ ! -d "$basedir" ]
 	then		
 		echo "$basedir not a directory" >&2
-		echo "$basedir not a directory" 1>>$l_logfile
+		echo "$basedir not a directory" 2>>$log_errors
 		exit $e_notDirectory
 	fi	
 }
@@ -53,8 +56,8 @@ change2BaseDir () {
 	if [ ! "$basedir" == `pwd` ]
 	then
 		echo "Not in target Base directory" >&2
-		echo "Not in target Base directory" 1>>$l_logfile
-		exit e_unable2Change2BaseDir
+		echo "Not in target Base directory" 2>>$l_logfile
+		exit $e_unable2Change2BaseDir
 	fi
 	echo "In target Base directory" >&2
 	echo "Current directory = `pwd`" >&2
@@ -90,7 +93,7 @@ initialiseVariables () {
 }
 
 goinside () {
-	echo "Let's go inside each project and run gradle -q check" >&2
+	echo "Visiting each project... " >&2
 	
 	for dir in `echo *`
 	do	
@@ -115,7 +118,7 @@ can_cd()
 	if ! cd "$dir"
 	then
 		echo "Not able to change into directory $dir" >&2
-		echo "Not able to change into directory $dir" 1>>$l_logfile
+		echo "Not able to change into directory $dir" 2>>$log_errors
 		return 1
 	else
 		return 0
@@ -127,10 +130,10 @@ is_gradleProject() {
 	if [ ! -f "build.gradle" ] 
 	then
 		echo "Not a Gradle project" >&2
-		echo "Not a Gradle project" 1>>$l_logfile
+		echo "Not a Gradle project" 2>>$log_errors
 		return 1
 	else
-		ls build.gradle &>>$l_logfile
+		#ls build.gradle 1>>$l_logfile
 		return 0
 	fi	
 }
@@ -140,7 +143,7 @@ updateWC () {
 	then
 		echo "Let's update your working copy" >&2
 		echo "Let's update your working copy" 1>>$l_logfile
-		git pull &>>$l_logfile
+		git pull 1>>$l_logfile 2>>$log_errors
 	fi	
 }
 
@@ -156,14 +159,14 @@ execute_gradle_tasks () {
 		echo "Let's execute gradle $defaultGradleTasks" >&2
 		echo "Let's execute gradle $defaultGradleTasks" 1>>$l_logfile
 		#gradle -q check
-		gradle $defauleGradleTasks &>>$l_logfile 
+		gradle $defauleGradleTasks 1>>$l_logfile 2>>$log_errors
 	fi	
 }
 
 stopGradleDaemons () {
-	echo "Stopping all Gradle's Daemon(s)" 1>>$l_logfile
+	echo "Stopping all Gradle's Daemon(s)" >&2 1>>$l_logfile
 	#gradle -v --stop
-	gradle --stop &>>$l_logfile
+	gradle --stop 1>>$l_logfile
 }
 
 runProjects () {
@@ -174,9 +177,52 @@ runProjects () {
 		return 0
 	fi
 	echo "Changing to boot project " >&2
-	cd $p_bootproject &>>$l_logfile
+	cd $p_bootproject >&2
 	echo "Current directory= `pwd`" >&2
-	exec ./boot.sh & &>>$l_logfile
+	local log_registry=$PWD/registry.log
+	local log_cardesire=$PWD/cardesire.log
+	local log_payment=$PWD/payment.log
+	local log_dealer=$PWD/dealer.log
+ 	: > $log_registry
+ 	: > $log_cardesire
+ 	: > $log_payment
+ 	: > $log_dealer	
+
+	echo "Registry" >&2 
+	gradle -Pmsname=registry runMS & >&$log_registry #2>&$log_registry
+	sleep 5
+	echo "Cardesire" >&2
+	gradle -Pmsname=cardesire runMS & >&$log_cardesire # 2>&$log_cardesire
+	sleep 2
+	echo "Dealer" >&2
+	gradle -Pmsname=dealer runMS & >&$log_dealer # 2>&$log_dealer
+	sleep 2
+	echo "Payment" >&2
+	gradle -Pmsname=payment runMS & >&$log_payment # 2>&$log_payment
+	sleep 2	
+}
+
+startDockerContainers () {
+	exec $d_startedin/docker_daemon_start.sh & >&2
+	while ! docker system info &>/dev/null
+	do
+		(( i++ == 0 )) && printf %s ' -- Docker Daemon is starting ....' || printf '.'
+		sleep 1
+	done
+	(( i )) && printf '\n'
+	echo "Docker is ready" >&2
+	docker start some-rabbit mongodb ecomgw-postgres & 1>>$l_logfile 2>>$log_errors
+ 	#exec $d_startedin/docker_containers_start.sh & >&2	
+}
+
+startFrontEnd () {
+	echo "starting front end..." >&2
+	cd $basedir/$1
+	echo "pwd=`pwd`" >&2 
+	local log_npm_start=$PWD/npm_start.log
+	: > $log_npm_start
+	npm start & 1>>$log_npm_start 2>>$log_npm_start	
+	popd
 }
 
 assurePWDisBaseDir () {
@@ -184,13 +230,13 @@ assurePWDisBaseDir () {
 	if [ `pwd` != "$basedir" ]
 	then
 		echo "Changing to Base directory" >&2
-		cd $basedir &>>$l_logfile
+		cd $basedir 1>>$l_logfile
 	fi
 	echo "After changed to Base directory..." >&2
 	if [ `pwd` != "$basedir"  ]
 	then
 		echo "Unable to change to Base directory" >&2
-		echo "Unable to change to Base directory" 1>>$l_logfile
+		echo "Unable to change to Base directory" 2>>$log_errors
 		return 1
 	fi
 	echo "Current directory= `pwd`" >&2
@@ -202,7 +248,7 @@ printStats () {
 	echo "Total projects visited = $numdirs" >&2 #1>>$l_logfile
 	echo "Total gradle projects visited = $num_gradleProjects" >&2 #1>>$l_logfile
 }
-
+  
 #checkArgs
 cleanLogs
 checkProvidedDir $1
@@ -212,8 +258,11 @@ initialiseVariables
 shouldUpdateWC $2
 stopGradleDaemons
 goinside 0
-runProjects 
-printStats
+startDockerContainers
+runProjects 0 
+startFrontEnd $4
+printStats 0
+
 echo 
 echo "Job done." >&2
 echo "Job done." 1>>$l_logfile
