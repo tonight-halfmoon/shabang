3969ac7f3a166904f7188f5e8f0f0fa752ba1872
A ELGA <Amado2@scuba.speedport.ip>
2020-01-05 15:31:22 +0000

    Use Solarized for FreeBSD

--- macos/emacs/organised/mmccs/colorthemeccs.el
+++ macos/emacs/organised/mmccs/colorthemeccs.el
@@ -1,5 +1,5 @@
 ;;; package --- Summary:
-;;; Customise `Colors'
+;;; Customise `Theme'
 ;; Package-Requires:
 ;;; Commentary:
 ;;
@@ -7,7 +7,11 @@
 ;;
 ;;; Code:
 
-;;
+(require 'package)
+
+;; ------------
+;; `Nord-theme'
+;; ============
 (cond ((string-equal system-type "darwin")
        (progn (unless package-archive-contents (package-refresh-contents))
               (unless (package-installed-p 'nord-theme)
@@ -15,10 +19,19 @@
               (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
               (load-theme 'nord t))))
 
-;; --------------------------------
-;; `Color-Theme-Sanityinc-Tomorrow'
-;; ================================
-;; (require 'color-theme-sanityinc-tomorrow)
+;; -----------------------
+;; `Color-Theme-Solarized'
+;; =======================
+;; Requirements
+;; (1) Clone the repository as follows:
+;; `cd ~/.emacs.d`
+;; `git clone https://github.com/sellout/emacs-color-theme-solarized.git`
+;; '(custom-enabled-themes (quote (solarized)))
+
+(cond ((string-equal system-type "berkeley-unix")
+       (progn (add-to-list 'custom-theme-load-path (expand-file-name
+                                                    "~/.emacs.d/emacs-color-theme-solarized"))
+              (load-theme 'solarized t))))
 
 ;; --------------------
 ;; `Color-Theme-Modern'
@@ -41,17 +54,20 @@
 ;; Reference [](http://nongnu.org/color-theme)
 ;; wget http://download.savannah.nongnu.org/releases/color-theme/color-theme-6.6.0.tar.gz
 ;; `7z x color-theme-6.6.0.tar.gz` into the `color-theme' directory
-(cond ((string-equal system-type "berkeley-unix")
-       (progn (add-to-list 'load-path "~/.emacs.d/color-theme")
-              (require 'color-theme)
-              ;; customised theme source "~/.emacs.d/color-theme/themes/color-theme-amado.el"
-              (with-eval-after-load "color-theme" '(progn (color-theme-initialize)
-                                                          (color-theme-amado)))
-              (custom-set-faces '(mode-line-buffer-id ((t
-                                                        (:background "black"
-                                                                     :foreground "magenta"
-                                                                     :weight extra-bold
-                                                                     :height 0.9))))))))
+;; To enable this theme set environment variable `theme` to "amado"
+
+(when (and (string-equal system-type "berkeley-unix")
+           (string-equal (getenv "theme") "amado"))
+  (progn (add-to-list 'load-path "~/.emacs.d/color-theme")
+         (require 'color-theme)
+         ;; customised theme source "~/.emacs.d/color-theme/themes/color-theme-amado.el"
+         (with-eval-after-load "color-theme" '(progn (color-theme-initialize)
+                                                     (color-theme-amado)))
+         (custom-set-faces '(mode-line-buffer-id ((t
+                                                   (:background "black"
+                                                                :foreground "magenta"
+                                                                :weight extra-bold
+                                                                :height 0.9)))))))
 
 ;; -----------
 ;; `Mode-line'
