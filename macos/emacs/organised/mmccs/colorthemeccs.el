6d5b2a8b1e50df9b9eaead5c9518c03d724f1a38
A ELGA <Amado2@scuba.speedport.ip>
2020-01-08 03:32:03 +0000

    Look tomorrow

--- macos/emacs/organised/mmccs/colorthemeccs.el
+++ macos/emacs/organised/mmccs/colorthemeccs.el
@@ -5,62 +5,105 @@
 ;;
 ;; Choose a library
 ;;
+;; Usage guide:
+;;
+;; Before starting a Emacs Server instance
+;; set environment variable `theme` as follows:
+;;
+;; Setting Environment Variable:
+;;
+;; Option (1)
+;; `export theme=amado`
+;;
+;; Option (2)
+;;
+;; `env theme=amado Emacs`
+;;
 ;;; Code:
 
-(require 'package)
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes"))
 
-;; ------------
-;; `Nord-theme'
-;; ============
-(cond ((string-equal system-type "darwin")
-       (progn (unless package-archive-contents (package-refresh-contents))
-              (unless (package-installed-p 'nord-theme)
-                (package-install 'nord-theme))
-              (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
-              (load-theme 'nord t))))
-
-;; -----------------------
-;; `Color-Theme-Solarized'
-;; =======================
-;; Requirements
-;; (1) Clone the repository as follows:
-;; `cd ~/.emacs.d`
-;; `git clone https://github.com/sellout/emacs-color-theme-solarized.git`
-;; '(custom-enabled-themes (quote (solarized)))
-
-(cond ((string-equal system-type "berkeley-unix")
-       (progn (add-to-list 'custom-theme-load-path (expand-file-name
-                                                    "~/.emacs.d/emacs-color-theme-solarized"))
-              (load-theme 'solarized t))))
+(when (string-equal (getenv "emacs_theme") nil)
+  (setenv "emacs_theme" "tomorrow"))
 
 ;; --------------------
-;; `Color-Theme-Modern'
+;; `Sanityinc-Tomorrow'
 ;; ====================
-;; Reference: [](https://github.com/emacs-jp/replace-colorthemes)
+;;
+;; Reference:
+;; [](https://github.com/purcell/color-theme-sanityinc-tomorrow)
 
-(cond ((string-equal system-type "darwin")
-       (progn (unless package-archive-contents (package-refresh-contents))
-              (unless (package-installed-p 'color-theme-modern)
-                (package-install 'color-theme-modern))
-              ;; (load-theme 'julie t t) ;; hober ;; ld-dark ;; oswald ;; matrix ;; 'railscast 'dark-font-lock
-              ;; (enable-theme 'julie)
-              ;; (load-theme 'dark-font-lock t t)
-              ;; (enable-theme 'dark-font-lock)
-              )))
+(when (string-equal (getenv "emacs_theme") "tomorrow")
+  (progn (add-to-list 'package-pinned-packages '(color-theme-sanityinc-tomorrow . "melpa") t)
+         (unless package-archive-contents (package-refresh-contents))
+         (unless (package-installed-p 'color-theme-sanityinc-tomorrow)
+           (package-install 'color-theme-sanityinc-tomorrow))
+         (load-theme 'sanityinc-tomorrow-night t)
+         (when (string-equal (getenv "emacs_background") "bright")
+           (add-to-list 'default-frame-alist '(background-color . "brightblack")))))
 
-;; -------------
-;; `Color-Theme'
-;; =============
-;; Reference [](http://nongnu.org/color-theme)
+;; ------------
+;; `Nord'
+;; ============
+
+(when (string-equal (getenv "emacs_theme") "nord")
+  (progn (unless package-archive-contents (package-refresh-contents))
+         (unless (package-installed-p 'nord-theme)
+           (package-install 'nord-theme))
+         (load-theme 'nord t)))
+
+;; -----------------------
+;; `Solarized' 1
+;; =======================
+;;
+;; git clone https://github.com/sellout/emacs-color-theme-solarized.git
+
+(when (string-equal (getenv "emacs_theme") "solarized1")
+  (progn (add-to-list 'custom-theme-load-path (expand-file-name
+                                               "~/.emacs.d/emacs-color-theme-solarized"))
+         (load-theme 'solarized t)))
+
+;; -----------------
+;; `Solarized' 2
+;; =================
+
+(when (string-equal (getenv "emacs_theme") "solarized2")
+  (progn (unless package-archive-contents (package-refresh-contents))
+         (unless (package-installed-p 'solarized-theme)
+           (package-install 'solarized-theme))
+         (load-theme 'solarized-dark t)))
+
+;; --------------------
+;; `Modern'
+;; ====================
+;;
+;; Reference:
+;; [](https://github.com/emacs-jp/replace-colorthemes)
+;; Start Emacs with this theme as follows:
+;; `env theme=modern emacs`
+
+(when (string-equal (getenv "emacs_theme") "modern")
+  (progn (unless package-archive-contents (package-refresh-contents))
+         (unless (package-installed-p 'color-theme-modern)
+           (package-install 'color-theme-modern))
+         ;;julie ;; hober ;; ld-dark ;; oswald ;; matrix ;; 'railscast 'dark-font-lock
+         (load-theme 'railscast t t)
+         (enable-theme 'railscast)))
+
+;; -------
+;; `amado'
+;; =======
+;;
+;; Reference:
+;; [](http://nongnu.org/color-theme)
+;;
 ;; wget http://download.savannah.nongnu.org/releases/color-theme/color-theme-6.6.0.tar.gz
 ;; `7z x color-theme-6.6.0.tar.gz` into the `color-theme' directory
-;; To enable this theme set environment variable `theme` to "amado"
 
-(when (and (string-equal system-type "berkeley-unix")
-           (string-equal (getenv "theme") "amado"))
+(when (string-equal (getenv "emacs_theme") "amado")
   (progn (add-to-list 'load-path "~/.emacs.d/color-theme")
          (require 'color-theme)
-         ;; customised theme source "~/.emacs.d/color-theme/themes/color-theme-amado.el"
+         (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/color-theme/themes"))
          (with-eval-after-load "color-theme" '(progn (color-theme-initialize)
                                                      (color-theme-amado)))
          (custom-set-faces '(mode-line-buffer-id ((t
@@ -68,10 +111,10 @@
                                                                 :foreground "magenta"
                                                                 :weight extra-bold
                                                                 :height 0.9)))))))
-
 ;; -----------
 ;; `Mode-line'
 ;; -----------
+
 (defun customise-mode-line-erlang-mode-hook ()
   "Customise Mode Line hook."
   (set-face-background 'mode-line "yellow"))
