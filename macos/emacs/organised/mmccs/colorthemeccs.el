204b1de79fe2878a4ebd05d8c2dbfef98cfbfc79
Ahmad Elghafari <ahmadelghafari@gmail.com>
2020-01-25 16:05:03 +0000

    Clean up

--- macos/emacs/organised/mmccs/colorthemeccs.el
+++ macos/emacs/organised/mmccs/colorthemeccs.el
@@ -32,47 +32,27 @@
 ;; [](https://github.com/purcell/color-theme-sanityinc-tomorrow)
 
 (when (string-equal (getenv "emacs_theme") "tomorrow")
-  (progn (eval-when-compile
-           (require 'package))
-         (add-to-list 'package-pinned-packages '(color-theme-sanityinc-tomorrow . "melpa") t)
-         (unless package-archive-contents (package-refresh-contents))
-         (unless (package-installed-p 'color-theme-sanityinc-tomorrow)
-           (package-install 'color-theme-sanityinc-tomorrow))
-         (load-theme 'sanityinc-tomorrow-night t)
-         (add-to-list 'default-frame-alist '(background-color . black))
-         (when (string-equal (getenv "emacs_background") "bright")
-           (add-to-list 'default-frame-alist '(background-color . "#4d4d4d")))))
-
-;; ------------
-;; `Nord'
-;; ============
-
-(when (string-equal (getenv "emacs_theme") "nord")
-  (progn (unless package-archive-contents (package-refresh-contents))
-         (unless (package-installed-p 'nord-theme)
-           (package-install 'nord-theme))
-         (load-theme 'nord t)))
+  (add-to-list 'package-pinned-packages '(color-theme-sanityinc-tomorrow . "melpa") t)
+  (unless package-archive-contents (package-refresh-contents))
+  (unless (package-installed-p 'color-theme-sanityinc-tomorrow)
+    (package-install 'color-theme-sanityinc-tomorrow))
+  (load-theme 'sanityinc-tomorrow-night t)
+  (add-to-list 'default-frame-alist '(background-color . black))
+  (when (string-equal (getenv "emacs_background") "bright")
+    (add-to-list 'default-frame-alist '(background-color . "#4d4d4d")))
+  (enable-theme 'sanityinc-tomorrow-night))
 
 ;; -----------------------
-;; `Solarized' 1
+;; `Solarized'
 ;; =======================
 ;;
 ;; git clone https://github.com/sellout/emacs-color-theme-solarized.git
 
-(when (string-equal (getenv "emacs_theme") "solarized1")
-  (progn (add-to-list 'custom-theme-load-path (expand-file-name
-                                               "~/.emacs.d/emacs-color-theme-solarized"))
-         (load-theme 'solarized t)))
-
-;; -----------------
-;; `Solarized' 2
-;; =================
-
-(when (string-equal (getenv "emacs_theme") "solarized2")
-  (progn (unless package-archive-contents (package-refresh-contents))
-         (unless (package-installed-p 'solarized-theme)
-           (package-install 'solarized-theme))
-         (load-theme 'solarized-dark t)))
+(when (string-equal (getenv "emacs_theme") "solarized")
+  (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/emacs-color-theme-solarized"))
+  (load-theme 'solarized t)
+  (set-variable 'solarized-termcolors 256)
+  (enable-theme 'solarized))
 
 ;; --------------------
 ;; `Modern'
@@ -84,82 +64,78 @@
 ;; `env theme=modern emacs`
 
 (when (string-equal (getenv "emacs_theme") "modern")
-  (progn (unless package-archive-contents (package-refresh-contents))
-         (unless (package-installed-p 'color-theme-modern)
-           (package-install 'color-theme-modern))
-         (cond ((string-equal (getenv "emacs_theme_modern_mode") "tonight")
-                (load-theme 'midnight t t)
-                (enable-theme 'midnight))
-               ((string-equal (getenv "emacs_theme_modern_mode") "dark")
-                (load-theme 'tty-dark t t)
-                (enable-theme 'tty-dark))
-               ((string-equal (getenv "emacs_theme_modern_mode") "lethe")
-                (load-theme 'lethe t t)
-                (enable-theme 'lethe))
-               ((string-equal (getenv "emacs_theme_modern_mode") "hober")
-                (load-theme 'hober t t)
-                (enable-theme 'hober))
-               ((string-equal (getenv "emacs_theme_modern_mode") "charcoal")
-                (load-theme 'charcoal-black t t)
-                (enable-theme 'charcoal-black))
-               ((string-equal (getenv "emacs_theme_modern_mode") "black-on-gray")
-                (load-theme 'black-on-gray t t)
-                (enable-theme 'black-on-gray))
-               ((string-equal (getenv "emacs_theme_modern_mode") "clarity")
-                (load-theme 'clarity t t)
-                (enable-theme 'clarity))
-               ((string-equal (getenv "emacs_theme_modern_mode") "desert")
-                (load-theme 'desert t t)
-                (enable-theme 'desert))
-               ((string-equal (getenv "emacs_theme_modern_mode") "gray30")
-                (load-theme 'gray30 t t)
-                (enable-theme 'gray30))
-               ((string-equal (getenv "emacs_theme_modern_mode") "late-night")
-                (load-theme 'late-night t t)
-                (enable-theme 'late-night))
-               ((string-equal (getenv "emacs_theme_modern_mode") "matrix")
-                (load-theme 'matrix t t)
-                (enable-theme 'matrix))
-               ((string-equal (getenv "emacs_theme_modern_mode") "renegade")
-                (load-theme 'renegade t t)
-                (enable-theme 'renegade))
-               (t (load-theme 'taming-mr-arneson t t)
-                  (enable-theme 'taming-mr-arneson)
-                  (set-face-attribute 'minibuffer-prompt t
-                                      :foreground "#5fffff")))
-         (cond ((string-equal (getenv "frame_background_mode") "light")
-                (setq frame-background-mode 'light))
-               (t
-                (setq frame-background-mode 'dark)))
-         (cond ((string-equal system-type "berkeley-unix")
-                (set-face-attribute 'minibuffer-prompt nil
-                                    :foreground "cyan"))
-               (t ()))
-         ;; background colour
-         (cond ((string-equal (getenv "emacs_background" ) "bright")
-                (add-to-list 'default-frame-alist '(background-color . "#4d4d4d")))
-               (t (add-to-list 'default-frame-alist '(background-color . black))))))
+  (unless package-archive-contents (package-refresh-contents))
+  (unless (package-installed-p 'color-theme-modern)
+    (package-install 'color-theme-modern))
+  (cond ((string-equal (getenv "emacs_theme_modern_mode") "tonight")
+         (load-theme 'midnight t t)
+         (enable-theme 'midnight))
+        ((string-equal (getenv "emacs_theme_modern_mode") "dark")
+         (load-theme 'tty-dark t t)
+         (enable-theme 'tty-dark))
+        ((string-equal (getenv "emacs_theme_modern_mode") "lethe")
+         (load-theme 'lethe t t)
+         (enable-theme 'lethe))
+        ((string-equal (getenv "emacs_theme_modern_mode") "hober")
+         (load-theme 'hober t t)
+         (enable-theme 'hober))
+        ((string-equal (getenv "emacs_theme_modern_mode") "charcoal")
+         (load-theme 'charcoal-black t t)
+         (enable-theme 'charcoal-black))
+        ((string-equal (getenv "emacs_theme_modern_mode") "black-on-gray")
+         (load-theme 'black-on-gray t t)
+         (enable-theme 'black-on-gray))
+        ((string-equal (getenv "emacs_theme_modern_mode") "clarity")
+         (load-theme 'clarity t t)
+         (enable-theme 'clarity))
+        ((string-equal (getenv "emacs_theme_modern_mode") "desert")
+         (load-theme 'desert t t)
+         (enable-theme 'desert))
+        ((string-equal (getenv "emacs_theme_modern_mode") "gray30")
+         (load-theme 'gray30 t t)
+         (enable-theme 'gray30))
+        ((string-equal (getenv "emacs_theme_modern_mode") "late-night")
+         (load-theme 'late-night t t)
+         (enable-theme 'late-night))
+        ((string-equal (getenv "emacs_theme_modern_mode") "matrix")
+         (load-theme 'matrix t t)
+         (enable-theme 'matrix))
+        ((string-equal (getenv "emacs_theme_modern_mode") "renegade")
+         (load-theme 'renegade t t)
+         (enable-theme 'renegade))
+        (t (load-theme 'taming-mr-arneson t t)
+           (enable-theme 'taming-mr-arneson)
+           (set-face-attribute 'minibuffer-prompt t
+                               :foreground "#5fffff")))
+  (cond ((string-equal (getenv "frame_background_mode") "light")
+         (setq frame-background-mode 'light))
+        (t
+         (setq frame-background-mode 'dark)))
+  (cond ((string-equal system-type "berkeley-unix")
+         (set-face-attribute 'minibuffer-prompt nil
+                             :foreground "cyan"))
+        (t ()))
+  ;; background colour
+  (cond ((string-equal (getenv "emacs_background" ) "bright")
+         (add-to-list 'default-frame-alist '(background-color . "#4d4d4d")))
+        (t (add-to-list 'default-frame-alist '(background-color . black)))))
 
 (when (string-equal (getenv "emacs_theme") "nimbus")
-  (progn (eval-when-compile
-           (require 'package))
-         (add-to-list 'package-pinned-packages '(nimbus-theme . "melpa") t)
-         (unless package-archive-contents (package-refresh-contents))
-         (unless (package-installed-p 'nimbus-theme)
-           (package-install 'nimbus-theme))
-         (load-theme 'nimbus t)))
+  (add-to-list 'package-pinned-packages '(nimbus-theme . "melpa") t)
+  (unless package-archive-contents (package-refresh-contents))
+  (unless (package-installed-p 'nimbus-theme)
+    (package-install 'nimbus-theme))
+  (load-theme 'nimbus t))
 
 (when (string-equal (getenv "emacs_theme") "ample")
-  (progn (eval-when-compile
-           (require 'package))
-         (add-to-list 'package-pinned-packages '(ample-theme . "melpa") t)
-         (unless package-archive-contents (package-refresh-contents))
-         (unless (package-installed-p 'ample-theme)
-           (package-install 'ample-theme))
-         (load-theme 'ample-flat t t)
-         (set-face-attribute 'region t
-                             :background "#808080")
-         (enable-theme 'ample-flat)))
+  (add-to-list 'package-pinned-packages '(ample-theme . "melpa") t)
+  (unless package-archive-contents (package-refresh-contents))
+  (unless (package-installed-p 'ample-theme)
+    (package-install 'ample-theme))
+  (load-theme 'ample-flat t t)
+  (set-face-attribute 'region t
+                      :background "#808080")
+  (enable-theme 'ample-flat))
 
 ;; -----------
 ;; `Mode-line'
