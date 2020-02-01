e49cc98f6c69742ba08826206f99429d3d50fd24
Ahmad Elghafari <ahmadelghafari@gmail.com>
2020-02-01 01:29:06 +0100

    Use Colour Theme Less

--- macos/emacs/organised/mmccs/themelessccs.el
+++ macos/emacs/organised/mmccs/themelessccs.el
@@ -1,10 +1,9 @@
-;;; package --- Summary:
-;;; Use 'Theme-`less'
+;;; package --- Summary
+;;; Setup `Theme-Less'
 ;; Package-Requires:
 ;;; Commentary:
-;;
 ;; Reference:
-;; []()
+;; [](https://jblevins.org/log/color-theme-less)
 ;;
 ;; Start Emacs with this theme as follows:
 ;;
@@ -16,86 +15,144 @@
 ;;
 ;;; Code:
 
-(defvar ccs-gray-12s "#121212")
+(defun ccs-error ()
+  "Configure face error."
+  (set-face-attribute 'error nil
+                      :underline nil
+                      :foreground "#ff00af"))
 
-(when (string-equal system-type "berkeley-unix")
-  (setq ccs-gray-12s "#d7ffd7"))
+(defun ccs-warning ()
+  "Configure face warning."
+  (set-face-attribute 'warning nil
+                      :underline nil
+                      :foreground "#ff87af"))
+
+(defun ccs-ido-first-match ()
+  "Configure ido first match."
+  (with-eval-after-load 'ido
+    ;;
+    (set-face-attribute 'ido-first-match nil
+                        :foreground "#87afaf"
+                        :inherit nil)))
+
+(defun ccs-ido-subdir ()
+  "Configure 'ido-subdir."
+  (with-eval-after-load 'ido
+    ;;
+    (set-face-attribute 'ido-subdir nil
+                        :foreground "#87afaf")))
 
 (defun ccs-hl-line ()
   "Configure hl-line."
   (with-eval-after-load 'hl-line
     ;;
-    (set-face-background 'hl-line ccs-gray-12s)))
+    (set-face-background 'hl-line "#121212")))
+
+(defun ccs-dired ()
+  "Configure dired."
+  (with-eval-after-load 'dired
+    ;;
+    (set-face-attribute 'dired-directory t
+                        :foreground "#5fd7ff")))
+
+(defun ccs-swiper ()
+  "Configure swiper."
+  (with-eval-after-load 'swiper
+    ;;
+    (set-face-attribute 'swiper-line-face t
+                        :foreground "#a8a8a8"
+                        :background "#303030")))
+
+(defun ccs-region ()
+  "Configure region."
+  (set-face-attribute 'region nil
+                      :inherit nil
+                      :weight 'bold
+                      :foreground "#9e9e9e"
+                      :background "#585858"))
+
+(defun ccs-highlight-changes ()
+  "Configure highlight-changes."
+  (eval-when-compile
+    (require 'hilit-chg))
+  (with-eval-after-load 'hilit-chg
+    ;;
+    (setq highlight-changes-face-list nil)
+    ;;
+    (set-face-attribute 'highlight-changes nil
+                        :foreground "#afff87")
+    ;;
+    (set-face-attribute 'highlight-changes-delete nil
+                        :underline nil
+                        :inherit nil
+                        :foreground "111")))
 
 (defun ccs-highlight-indentation ()
   "Configure highlight-indentation."
-  ;;
   (with-eval-after-load 'highlight-indentation
-    (require 'highlight-indentation)
-    (set-face-background 'highlight-indentation-face "#b2b2b2")
-    (set-face-background 'highlight-indentation-current-column-face ccs-gray-12s)))
-
-(defun ccs-highlight-changes()
-  "Configure Highlight-Changes."
-  (with-eval-after-load 'hilit-chg
     ;;
-    (set-face-attribute 'highlight-changes nil
-                        :foreground "#d7d7ff")))
+    (set-face-background 'highlight-indentation-face "#87afaf")
+    (set-face-background 'highlight-indentation-current-column-face "#87afaf")))
 
-(defun ccs-company-preview()
-  "Configure company."
-  (with-eval-after-load 'company
-    (require 'company)
-    (set-face-attribute 'company-preview t
-                        :background "#444444"
-                        :foreground "#808080")
-    (set-face-attribute 'company-preview-common t
-                        :background "#262626"
-                        :foreground "#cd00cd")
-    (set-face-attribute 'company-preview-search t
-                        :background "#0000cd"
-                        :foreground "#808080")
-    (set-face-attribute 'company-scrollbar-bg t
-                        :background "#808080")
-    (set-face-attribute 'company-scrollbar-fg t
-                        :background "#626262")
-    (set-face-attribute 'company-tooltip t
-                        :background "#444444"
-                        :foreground "#808080")
-    (set-face-attribute 'company-tooltip-selection t
-                        :background "#bcbcbc"
-                        :foreground "#00ff00"
-                        :weight 'bold)))
+(defun ccs-flyspell ()
+  "Configure Fly Spell."
+  (eval-when-compile
+    (require 'flyspell))
+  (with-eval-after-load 'flyspell
+    ;;
+    (set-face-attribute 'flyspell-duplicate nil
+                        :underline nil
+                        :foreground nil
+                        :background nil
+                        :inherit nil
+                        :inverse-video nil)
+    (set-face-attribute 'flyspell-incorrect t
+                        :underline nil
+                        :inherit nil
+                        :inverse-video t)))
 
-(when (string-match "less" (getenv "emacs_theme"))
-  (when (not (file-directory-p "~/.emacs.d/themes"))
-    (mkdir "~/.emacs.d/themes" "-p"))
-  (unless (file-exists-p "~/.emacs.d/themes/less-theme.el")
-    (url-copy-file "https://jblevins.org/git/misc.git/plain/less-theme.el"
-                   "~/.emacs.d/themes/less-theme.el"))
-  (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes"))
-  (load-theme 'less t)
-  (message "Loading theme less...")
-  (when (string-equal system-type "berkeley-unix")
-    (message "Customise theme `less' for FreeBSD...")
-    (setq initial-frame-alist '((background-color . "#5f5f5f")))
-    (setq default-frame-alist '((background-color . "#5f5f5f")))
-    (set-background-color "#5f5f5f")
-    (ccs-highlight-changes)
+(defun ccs-flycheck ()
+  "Configure Flycheck."
+  (with-eval-after-load 'flycheck
+    ;;
+    (set-face-attribute 'flycheck-error nil
+                        :underline nil)
+    (set-face-attribute 'flycheck-warning nil
+                        :underline nil)
+    (set-face-attribute 'flycheck-info nil
+                        :underline nil)))
+
+(defun ccs-theme-less ()
+  "Theme less configure, load and enable."
+  (when (string-match "less" (getenv "emacs_theme"))
+    (when (not (file-directory-p "~/.emacs.d/themes"))
+      (mkdir "~/.emacs.d/themes" "-p"))
+    (when (not (file-exists-p "~/.emacs.d/themes/less-theme.el"))
+      (url-copy-file "https://jblevins.org/git/misc.git/plain/less-theme.el"
+                     "~/.emacs.d/themes/less-theme.el" t))
+    (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+    (load-theme 'less t)
+    (message "Loading theme Less...")
+    (ccs-warning)
+    (ccs-error)
     (ccs-highlight-indentation)
-    (ccs-company-preview))
-  (ccs-hl-line)
-  (enable-theme 'less))
+    (ccs-highlight-changes)
+    (ccs-flyspell)
+    (ccs-flycheck)
+    (ccs-swiper)
+    (ccs-region)
+    (ccs-dired)
+    (ccs-hl-line)
+    (ccs-ido-subdir)
+    (ccs-ido-first-match)
+    (enable-theme 'less)))
+
+(ccs-theme-less)
 
 (provide 'themelessccs)
 
 ;; Local Variables:
 ;; coding: utf-8
-;; buffer-file-coding-system: utf-8
-;; mode: emacs-lisp
-;; indent-tabs-mode: nil
-;; tab-width: 2
 ;; indent-tabs-mode: nil
 ;; End:
-;; ex: ts=2 sw=2 et ft=sh
 ;;; themelessccs ends here
